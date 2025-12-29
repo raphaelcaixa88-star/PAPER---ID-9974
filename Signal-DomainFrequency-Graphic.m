cd9f13ae7f86a95608b83aa5daac03a2448599d4
raphaelcaixa88-star <raphaelcaixa88@gmail.com>
2025-12-29 06:30:51 -0300

    Update Signal-DomainFrequency-Graphic.m

--- Signal-DomainFrequency-Graphic.m
+++ Signal-DomainFrequency-Graphic.m
@@ -1,8 +1,8 @@
 clear all;
 close all;
 clc;
-%Caracterização do sinal
-y1=load('.txt');
+%Signal characterization
+y1=load('Anesthetized.txt'); %Load signal from anesthetized rat
 fs = 500; % find the sampling rate or frequency
 T = 1/fs;% sampling rate or frequency
 % find the length of the data per second
@@ -11,21 +11,21 @@ ls = size(y1);
 t = (0 : N-1)/fs;% sampling period
 
 
-%%%%%%%% FILTROS PASSA BAIXA E PASSA ALTA
+%%%%%%%% Low-pass and High-pass filtering 
 
 [bl,al]=butter(10,10.0/250);
 y11=filtfilt(bl,al,y1);%low-pass
 [bh,ah]=butter(4,0.5/250,'high');
 y12=filtfilt(bh,ah,y11);%high-pass
 
-%%%%%%%% FILTRO NOT///CH 60HZ
+%Filter notch / 60 Hz
 
 d = designfilt('bandstopiir','FilterOrder',4, ...
                'HalfPowerFrequency1',58,'HalfPowerFrequency2',61, ...
                'DesignMethod','butter','SampleRate',fs);
            buttLoop =filtfilt(d,y12);
            
-%%%%%%%% LINHA DE BASE DO SINAL          
+%Baseline wander removal (polynomial detrending)  
 
 [p,s,mu] = polyfit((1:numel(buttLoop))',buttLoop,6);
 f_y = polyval(p,(1:numel(buttLoop))',[],mu);
@@ -35,9 +35,8 @@ ECG_data = buttLoop - f_y;        % Detrend data
 pp22=fft(ECG_data);
 xf=linspace(0,100,length(ECG_data));
 
+% Visualizationf of FFT Graphic
 figure(1)
-%plot((tths),'b-')
 plot(xf,abs(pp22),'black')
 xlabel('Frequency (Hz)'); ylabel('Amplitude (V)')
-% %axis([34.45 34.95 3.9 4.3]);
-% %axis([15 16.7 -0.5 0.5]);
+% axis([0 3.5]); Establish two equal frequency for both images anesthetized and awake.
